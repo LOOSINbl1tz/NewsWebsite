b10c427f1c16b7221d0168e394545fa1afa60338
unknown <rohitthakur1122rt34@gmail.com>
2023-10-17 01:22:09 +0530

    Still working

--- backend/dashboard/news_api_call.py
+++ backend/dashboard/news_api_call.py
@@ -6,8 +6,14 @@ class NewsFetcher:
         self.key = 'pub_31284c9a4a173267fcdea6f4e3ee581d42fb4'
         self.client = NewsDataApiClient(self.key)
         self.query = keyword
+
+    def getFiltered(self, data):
+        
+        return data
     def getNews(self):
         link = f'https://newsdata.io/api/1/news?apikey=pub_31284c9a4a173267fcdea6f4e3ee581d42fb4&q={self.query}'
         news_data = requests.get(link)
         data = news_data.json()['results']
+        data = self.getFiltered(data)
+
         return data
